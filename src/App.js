495b787ffad18c0ebcd3570522ef6eeba1f5d136
Palak Soni <online.palak@gmail.com>
2023-11-11 15:46:11 +0530

    Create Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { Routes, Route, Outlet, Link } from "react-router-dom";
 import './App.scss';
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import About from "./components/About";
+import Dashboard from "./components/Dashboard";
+import NoMatch from "./components/NoMatch";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +23,19 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="dashboard" element={<Dashboard />} />
+
+          {/* Using path="*"" means "match anything", so this route
+                acts like a catch-all for URLs that we don't have explicit
+                routes for. */}
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
